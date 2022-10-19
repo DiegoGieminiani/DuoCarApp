61bb74add65027c66d83d09e7ccc077d9f93c23b
DiegoGieminiani <87047008+DiegoGieminiani@users.noreply.github.com>
2022-10-19 00:34:02 -0300

    Cambios en perfil 2

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  username = this.route.snapshot.paramMap.get('userName');
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  toProfile() {
+    this.router.navigate([`/profile`]);
+  }
+
+  toTripHistory() {
+    this.router.navigate([`/trip-history`]);
+  }
+
+  toHome() {
+    this.router.navigate([`/home/${this.username}`]);
+  }
 }
